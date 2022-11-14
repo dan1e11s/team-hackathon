a5fb078c9d23f88c836f80c71b4bfeca80a7ad84
Даниел Муратов <danielmuratov2003@gmail.com>
2022-11-15 01:55:00 +0600

    add cart

---
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PaymentForm from '../../components/paymentForm/PaymentForm';
+
+const PaymentPage = () => {
+  return <PaymentForm />;
+};
+
+export default PaymentPage;
