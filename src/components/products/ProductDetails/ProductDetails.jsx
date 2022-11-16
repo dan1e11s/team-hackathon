38abc88b04e038bea49d3337f094e4c0d94014fa
User <saparalievabu@gmail.com>
2022-11-16 19:13:33 +0600

    adaptive details

--- src/components/products/ProductDetails/ProductDetails.jsx
+++ src/components/products/ProductDetails/ProductDetails.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useProducts } from '../../../contexts/ProductContextProvider';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import '../ProductDetails/ProductDetails.css';
-import Popper from '@mui/material/Popper';
-import Fade from '@mui/material/Fade';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import { IconButton } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useProducts } from "../../../contexts/ProductContextProvider";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import "../ProductDetails/ProductDetails.css";
+import Popper from "@mui/material/Popper";
+import Fade from "@mui/material/Fade";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { IconButton } from "@mui/material";
 
 export default function ProductDetails() {
   const { oneProduct, getOneProduct } = useProducts();
@@ -22,9 +22,9 @@ export default function ProductDetails() {
   const [open, setOpen] = useState(false);
   const [placement, setPlacement] = useState();
 
-  const handleClick = (newPlacement) => (event) => {
+  const handleClick = newPlacement => event => {
     setAnchorEl(event.currentTarget);
-    setOpen((prev) => placement !== newPlacement || !prev);
+    setOpen(prev => placement !== newPlacement || !prev);
     setPlacement(newPlacement);
   };
 
@@ -36,11 +36,10 @@ export default function ProductDetails() {
             open={open}
             anchorEl={anchorEl}
             placement={placement}
-            transition
-          >
+            transition>
             {({ TransitionProps }) => (
               <Fade {...TransitionProps} timeout={350}>
-                <Paper>
+                <Paper className="detailsPaper">
                   <Typography sx={{ p: 2 }}>
                     {oneProduct.abilitiesDesc}
                   </Typography>
@@ -49,84 +48,106 @@ export default function ProductDetails() {
             )}
           </Popper>
           <div
+            className="detailsMain"
             style={{
-              width: '75%',
-              color: '#999999',
-              padding: '20px 0',
-            }}
-          >
+              width: "75%",
+              color: "#999999",
+              padding: "20px 0",
+              height: "100%",
+            }}>
             <div
+              className="detailsPage"
               style={{
-                display: 'flex',
-                justifyContent: 'space-around',
-                alignItems: 'center',
-              }}
-            >
-              <div style={{ backgroundColor: '#101011', borderRadius: '20px' }}>
-                <img src={oneProduct.image.main} alt="image" />
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}>
+              <div
+                className="boxforImg"
+                style={{ backgroundColor: "#101011", borderRadius: "20px" }}>
+                <img
+                  className="detailsImg"
+                  src={oneProduct.image.main}
+                  alt="image"
+                />
               </div>
-              <div style={{ width: '25%' }}>
-                <h4 style={{ textAlign: 'center', fontSize: '25px' }}>
+              <div className="detailsbot" style={{ width: "25%" }}>
+                <h4
+                  className="detailsTitle"
+                  style={{ textAlign: "center", fontSize: "25px" }}>
                   {oneProduct.title}
                 </h4>
                 <p
+                  className="detailsDesc"
                   style={{
-                    fontWeight: '700',
-                    fontSize: '18px',
-                    marginBottom: '45px',
-                  }}
-                >
+                    fontWeight: "700",
+                    fontSize: "18px",
+                    marginBottom: "45px",
+                  }}>
                   {oneProduct.description}
                 </p>
                 <div
+                  className="detailsBox"
                   style={{
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    alignItems: 'center',
-                    backgroundColor: '#101011',
-                    borderTopLeftRadius: '20px',
-                    borderTopRightRadius: '20px',
-                    fontWeight: '700',
-                  }}
-                >
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center",
+                    backgroundColor: "#101011",
+                    borderTopLeftRadius: "20px",
+                    borderTopRightRadius: "20px",
+                    fontWeight: "700",
+                  }}>
                   <div>
-                    <p style={{ fontSize: '20px' }}>Height</p>
-                    <p style={{ textAlign: 'center' }}>{oneProduct.height}</p>
+                    <p className="detailsP" style={{ fontSize: "20px" }}>
+                      Height
+                    </p>
+                    <p className="detailsP" style={{ textAlign: "center" }}>
+                      {oneProduct.height}
+                    </p>
                   </div>
                   <div>
-                    <p style={{ fontSize: '20px' }}>Category</p>
-                    <p style={{ textAlign: 'center' }}>
+                    <p className="detailsP" style={{ fontSize: "20px" }}>
+                      Category
+                    </p>
+                    <p className="detailsP" style={{ textAlign: "center" }}>
                       {oneProduct.category[0].toUpperCase() +
                         oneProduct.category.slice(1)}
                     </p>
                   </div>
                 </div>
                 <div
+                  className="detailsBox"
                   style={{
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    alignItems: 'center',
-                    backgroundColor: '#101011',
-                    borderBottomLeftRadius: '20px',
-                    borderBottomRightRadius: '20px',
-                    fontWeight: '700',
-                  }}
-                >
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center",
+                    backgroundColor: "#101011",
+                    borderBottomLeftRadius: "20px",
+                    borderBottomRightRadius: "20px",
+                    fontWeight: "700",
+                  }}>
                   <div>
-                    <p style={{ fontSize: '20px' }}>Weight</p>
-                    <p style={{ textAlign: 'center' }}>{oneProduct.weight}</p>
+                    <p className="detailsP" style={{ fontSize: "20px" }}>
+                      Weight
+                    </p>
+                    <p className="detailsP" style={{ textAlign: "center" }}>
+                      {oneProduct.weight}
+                    </p>
                   </div>
                   <div
                     style={{
-                      textAlign: 'center',
-                    }}
-                  >
-                    <p style={{ fontSize: '20px' }}>Abilities</p>
-                    <p style={{ display: 'flex', alignItems: 'center' }}>
+                      textAlign: "center",
+                    }}>
+                    <p className="detailsP" style={{ fontSize: "20px" }}>
+                      Abilities
+                    </p>
+                    <p
+                      className="detailsP"
+                      style={{ display: "flex", alignItems: "center" }}>
                       {oneProduct.abilities}
                       <HelpOutlineIcon
                         className="help-icon"
-                        onClick={handleClick('bottom')}
+                        onClick={handleClick("bottom")}
                       />
                     </p>
                   </div>
@@ -136,7 +157,7 @@ export default function ProductDetails() {
           </div>
         </>
       ) : (
-        <h4 style={{ color: '#999999' }}>Loading...</h4>
+        <h4 style={{ color: "#999999" }}>Loading...</h4>
       )}
     </>
   );
