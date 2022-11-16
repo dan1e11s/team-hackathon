7c5a2e6cac965491f0c129370115d62bfa82fb39
Даниел Муратов <danielmuratov2003@gmail.com>
2022-11-16 18:52:23 +0600

    fixed add product page

--- src/components/products/AddProduct/AddProduct.jsx
+++ src/components/products/AddProduct/AddProduct.jsx
@@ -1,28 +1,33 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useProducts } from "../../../contexts/ProductContextProvider";
-import "../AddProduct/AddProduct.css";
-import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useProducts } from '../../../contexts/ProductContextProvider';
+import '../AddProduct/AddProduct.css';
+import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 
 const AddProduct = () => {
   const { addProduct } = useProducts();
   const navigate = useNavigate();
 
   const [product, setProduct] = useState({
-    title: "",
-    price: "",
-    description: "",
-    category: "",
-    image: "",
-    rating: "",
+    title: '',
+    price: '',
+    description: '',
+    category: '',
+    image: '',
+    rating: '',
   });
 
-  const handleInp = e => {
-    if (e.target.name === "price" && e.target.name === "rating") {
+  const handleInp = (e) => {
+    if (
+      e.target.name === 'price' &&
+      e.target.name === 'height' &&
+      e.target.name === 'weight'
+    ) {
       let obj = {
         ...product,
         price: Number(e.target.value),
-        rating: Number(e.target.value),
+        height: Number(e.target.value),
+        weight: Number(e.target.value),
       };
       setProduct(obj);
     } else {
@@ -37,8 +42,8 @@ const AddProduct = () => {
   return (
     <div className="add-main">
       <CatchingPokemonIcon
-        sx={{ fontSize: "35px", ml: "6%", mt: "2.5%", cursor: "pointer" }}
-        onClick={() => navigate("/product")}
+        sx={{ fontSize: '35px', ml: '6%', mt: '2.5%', cursor: 'pointer' }}
+        onClick={() => navigate('/product')}
       />
       <div className="add-page">
         <form className="add-form">
@@ -81,16 +86,38 @@ const AddProduct = () => {
           <input
             className="input"
             type="text"
-            placeholder="Rating"
-            name="rating"
+            placeholder="Height"
+            name="height"
+            onChange={handleInp}
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="Weight"
+            name="weight"
+            onChange={handleInp}
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="Abilities"
+            name="abilities"
+            onChange={handleInp}
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="Abilities Description"
+            name="abilitiesDesc"
             onChange={handleInp}
           />
           <button
             className="button"
             onClick={() => {
               addProduct(product);
-              navigate("/admin");
-            }}>
+              navigate('/admin');
+            }}
+          >
             ADD
           </button>
         </form>
